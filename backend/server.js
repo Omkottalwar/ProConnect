08de3fb3d99b1238cfc77ba28f4e7e64aaadf695
omkottalwar <kottalwarom@gmail.com>
2025-11-11 17:56:52 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.use(cors({
-  origin: "http://localhost:3000", // your frontend origin
+  origin: "https://pro-connect-zeta-flame.vercel.app/", // your frontend origin
   credentials: true
 }));
 
