285f47914250e68d600fd873d9bc100fe0ffe405
Omkottalwar <kottalwarom@gmail.com>
2026-04-01 17:35:52 +0530

    Refactor index.jsx for improved formatting and structure

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -4,37 +4,47 @@ import styles from "@/styles/Home.module.css";
 import { Inter } from "next/font/google";
 import { useRouter } from "next/router";
 import UserLayout from "@/layout/UserLayout";
-const inter=Inter({subsets:["latin"]})
 
-
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const router=useRouter();
+  const router = useRouter();
+
   return (
     <>
-   
+      {/* ✅ META TAGS GO HERE */}
+      <Head>
+       <meta name="google-site-verification" content="PQ0RMPeXEJYGOYSWRddbw_7aMUgIu8z4bF_2dvdXEA4" />
+      
+      </Head>
+
       <UserLayout>
-   
-    <div className={styles.container}>
-      <div className={styles.mainContainer}>
-        <div className={styles.mainContainer__left}>
-          <p>Connnect with Friends without  Exaggeration </p>
-          <p>A True social media platform, with stories no blufs  </p>
-          <div onClick={()=>{
-            router.push("/login")
-          }} className={styles.buttonJoin}
-          > <p>Join Now</p></div>
+        <div className={styles.container}>
+          <div className={styles.mainContainer}>
+            <div className={styles.mainContainer__left}>
+              <p>Connnect with Friends without Exaggeration</p>
+              <p>A True social media platform, with stories no blufs</p>
 
+              <div
+                onClick={() => {
+                  router.push("/login");
+                }}
+                className={styles.buttonJoin}
+              >
+                <p>Join Now</p>
+              </div>
+            </div>
+
+            <div className={styles.mainContainer__right}>
+              <img
+                style={{ width: "90%" }}
+                src="images/linkedin-network.jpg"
+                alt="/"
+              />
+            </div>
+          </div>
         </div>
-        <div className={styles.mainContainer__right}>
-          <img style={{width:"90%"}} src="images/linkedin-network.jpg" alt="/" />
-
-        </div>
-
-      </div>
-    </div>
-    
-    </UserLayout>
-      </>
+      </UserLayout>
+    </>
   );
 }
